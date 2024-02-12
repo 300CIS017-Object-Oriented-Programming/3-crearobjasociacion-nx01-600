b8449d8b8321fb90f41bb05b958d70cc2b848ba1
NiCT06 <65934573+nx01-600@users.noreply.github.com>
2024-02-11 22:38:37 -0500

    Update Perro.h
    versión nueva y finalizada del archivo

--- Perro.h
+++ Perro.h
@@ -1,44 +1,42 @@
-//
-// Created by lufe0 y Gonzo Feb2024.
-//
 #ifndef PERRO_H
 #define PERRO_H
 
 #include <string>
 #include <iostream>
 #include "Propietario.h"
+#include "Veterinario.h"
+
+using namespace std;
 
 class Perro {
 private:
     int edad;
-    std::string nombre;
-    std::string raza;
-    std::string tamanio;
-    std::string color;
+    string nombre;
+    string raza;
+    string tamanio;
+    string color;
     Propietario* pPropietario;
+    Veterinario* pVeterinario;
 
 public:
-    Perro();
-    ~Perro(); //Destructor
+    Perro(int edad, string nombre, string raza, string tamanio, string color);
+    ~Perro();
     void ladrar();
-    Propietario * getPropietario();
-
-    // Sirve para.......
-    void agregarPropietario(std::string nombre, std::string docIdentidad);
-
-    // Sirve para relacionar dos clases cuando ya se tiene un objeto
-    void setPropietario(Propietario * pPropietario);
-
-
+    Propietario *getPropietario();
+    void agregarPropietario(string nombre, string docIdentidad);
+    void setPropietario(Propietario *pPropietario);
     int getEdad();
     void setEdad(int edad);
-    std::string getRaza();
-    void setRaza(std::string raza);
-    std::string getNombre();
-    void setNombre(std::string nombre);
-    std::string getTamanio();
-    void setTamanio(std::string tamanio);
-    std::string getColor();
-    void setColor(std::string color);
+    string getRaza();
+    void setRaza(string raza);
+    string getNombre();
+    void setNombre(string nombre);
+    string getTamanio();
+    void setTamanio(string tamanio);
+    string getColor();
+    void setColor(string color);
+    void asociarVeterinario(Veterinario* pVeterinario);
+    Veterinario* getVeterinario();
 };
+
 #endif
