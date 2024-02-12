9c6e5f5be6499c4915a97a96c49ea6f1c492ccaa
NiCT06 <65934573+nx01-600@users.noreply.github.com>
2024-02-11 22:54:00 -0500

    Update Propietario.h
    Versión nueva y finalizada del archivo

--- Propietario.h
+++ Propietario.h
@@ -1,27 +1,21 @@
-//
-// Created by lufe y Gonzo Feb2024.
-//
-
 #ifndef PROPIETARIO_H
 #define PROPIETARIO_H
 
 #include <string>
 
+using namespace std;
+
 class Propietario {
 private:
-    std::string nombre;
-    std::string docIdentidad;
+    string nombre;
+    string docIdentidad;
     int edad;
+
 public:
-    Propietario() = default; //Agrega constructor por defecto sin cuerpo
-    Propietario(std::string nombre, std::string docIdentidad);
+    Propietario(string nombre, string docIdentidad);
     void mostrarInfo();
-    std::string getNombre();
-    void setNombre(std::string nombre);
-
-
-
+    string getNombre();
+    void setNombre(string nombre);
 };
 
-
-#endif //PROPIETARIO_H
+#endif
