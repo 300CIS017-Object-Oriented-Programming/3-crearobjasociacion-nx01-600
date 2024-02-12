ed162b4aaa01189b615711b86103c172d7b45d02
NiCT06 <65934573+nx01-600@users.noreply.github.com>
2024-02-11 22:47:03 -0500

    Update main.cpp
    Versión nueva y finalizada del archivo

--- main.cpp
+++ main.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include "Perro.h"
 #include "Propietario.h"
+#include "Veterinario.h"
+
+using namespace std;
 
 int main() {
+    Raza mastinNapolitano("Mastin Napolitano", "Italia");
+    Raza maltes("Maltes", "Italia");
 
-    // Instanciar
-    Perro firulais;
-    firulais.setNombre("Firulais");
-    firulais.setColor("Negro");
-    firulais.setEdad(5);
-    firulais.setTamanio("Grande");
-    firulais.setRaza("Mastin napolitano");
-
-    //Agregar un nuevo pPropietario a Firulais
+    Perro firulais(5, "Firulais", "Mastin Napolitano", "Grande", "Negro");
     firulais.agregarPropietario("Carlos Zapata", "102842313");
 
-    // Consultar el nombre del pPropietario del perro Firulais
-    std::cout<< "El nombre del pPropietario del perro "<<firulais.getNombre() << " es " << firulais.getPropietario()->getNombre()<<std::endl;
+    Veterinario veterinarioFirulais("Dr. Pérez", 10);
+    firulais.asociarVeterinario(&veterinarioFirulais);
+
+    cout << "Nombre del Perro: " << firulais.getNombre() << endl;
+    cout << "Raza del Perro: " << firulais.getRaza() << endl;
+    cout << "Propietario: " << firulais.getPropietario()->getNombre() << endl;
+
+    cout << "Veterinario del Perro: " << firulais.getVeterinario()->getNombre() << endl;
+    cout << "Años de Experiencia del Veterinario: " << firulais.getVeterinario()->getAniosExperiencia() << endl;
 
     return 0;
 }
